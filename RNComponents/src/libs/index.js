e02d7e158cac1297860533cadd8b60f64692a669
varunon9 <varunon9@gmail.com>
2021-06-13 12:47:20 +0530

    exposed normalizeFontSize util

--- RNComponents/src/libs/index.js
+++ RNComponents/src/libs/index.js
@@ -2,5 +2,6 @@ import Text from './Text';
 import Heading from './Heading';
 import NavigationHeader from './NavigationHeader';
 import Icon from './icon/Icon';
+import { normalizeFontSize } from './util';
 
-export { Text, Heading, NavigationHeader, Icon };
+export { Text, Heading, NavigationHeader, Icon, normalizeFontSize };
